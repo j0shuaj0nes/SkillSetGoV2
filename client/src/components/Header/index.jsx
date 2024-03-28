2ac5ec63d1c66b950e88edb0ef4b0dc3852366ca
Diana Tran <diana.tran44@gmail.com>
2024-03-28 23:19:27 +1100

    updated dashboard

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,7 +1,16 @@
-import { Link } from 'react-router-dom';
-import { Container, Button, Title, Burger, Group, Anchor, MantineProvider,createTheme } from '@mantine/core';
-import Auth from '../../utils/auth';
-import classes from './header.css' 
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Button,
+  Title,
+  Burger,
+  Group,
+  Anchor,
+  MantineProvider,
+  createTheme,
+} from "@mantine/core";
+import Auth from "../../utils/auth";
+import classes from "./header.css";
 
 const Header = () => {
   const logout = (event) => {
@@ -13,50 +22,57 @@ const Header = () => {
     components: {
       Container: Container.extend({
         classNames: (_, { size }) => ({
-          root: ({ [classes.responsiveContainer]: size === 'responsive' }),
+          root: { [classes.responsiveContainer]: size === "responsive" },
         }),
       }),
     },
   });
 
-
   return (
-    <header className={classes.header}>
-  
-      <MantineProvider theme={theme}>
-        <Container size="responsive" bg="var(--mantine-color-blue-light)" className={classes.inner}>
-        <Group justify="flex-start">
-        <Title className={classes.title}>SkillSetGo</Title>
+    <div className={classes.header}>
+      <Container
+        size="responsive"
+        
+        className={classes.inner}
+      >
+        <Group justify="space-between">
+          <Title className={classes.title}>SkillSetGo</Title>
+          <div>
+
+            {Auth.loggedIn() ? (
+              <>
+                <Link className="btn btn-lg btn-info m-2" to="/me">
+                  {/* Run the getProfile() method to get access to the unencrypted token value in order to retrieve the user's username  */}
+                  {Auth.getProfile().authenticatedPerson.username}'s profile
+                </Link>
+                <Button
+                  variant="outline"
+                  color="gray"
+                  className="btn-lg m-2"
+                  onClick={logout}
+                >
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button me={2} variant="filled">
+                  <Anchor c="white" component={Link} to="/login">Login</Anchor>
+                </Button>
+                <Button  variant="filled">
+                  <Anchor c="white" component={Link} to="/signup">Signup</Anchor>
+                </Button>
+              </>
+            )}
+          </div>
         </Group>
-        <Group justify="flex-end" gap={5} visibleFrom="xs">
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
-                {/* Run the getProfile() method to get access to the unencrypted token value in order to retrieve the user's username  */}
-                {Auth.getProfile().authenticatedPerson.username}'s profile
-              </Link>
-              <Button variant="outline" color="gray" className="btn-lg m-2" onClick={logout}>
-                Logout
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button variant="filled">
-                <Anchor href="/login">Login</Anchor>
-              </Button>
-              <Button variant="filled">
-               <Anchor href="/signup">Signup</Anchor>
-              </Button>
-            </>
-          )}
-        </Group>
+   
         {/* <Group h="100%" px="md">
           <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
           <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
         </Group> */}
       </Container>
-      </MantineProvider>
-    </header>
+    </div>
   );
 };
 
